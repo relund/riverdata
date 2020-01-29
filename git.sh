a8a1b5785182c7a2c578fb010d7abc18ea3e9d1d
relund <junk@relund.dk>
2020-01-29 13:56:56 +0100

    add specific file

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 # Git commands for push
 git config --local user.name "relund"
 git config --local user.email "junk@relund.dk"
-git add .
+git add data_flow_sluse.csv
 git commit --allow-empty -m "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
 git push https://relund:$github_token@github.com/relund/skjern.git HEAD:master 
