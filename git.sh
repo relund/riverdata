e683efdf54af97e116f99fe1e735bc2dc38fa933
relund <junk@relund.dk>
2020-01-29 13:51:52 +0100

    fixed bug

--- git.sh
+++ git.sh
@@ -3,4 +3,4 @@ git config --local user.name "relund"
 git config --local user.email "junk@relund.dk"
 git add .
 git commit --allow-empty -m "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
-git push https://relund:$github_token@github.com/relund/testTravis.git HEAD:master 
+git push https://relund:$github_token@github.com/relund/skjern.git HEAD:master 
