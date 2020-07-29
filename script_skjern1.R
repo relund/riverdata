f97e5663b325ef2af3f6ee89aba6b9e0a6888785
relund <junk@relund.dk>
2020-07-29 21:05:58 +0200

    Update with functions

--- script_skjern1.R
+++ script_skjern1.R
@@ -1,187 +1,64 @@
+library(xml2)
 library(rvest)
 library(jsonlite)
 library(tidyverse)
 library(lubridate)
+library(zoo)
+
+source("functions.R")
 
 
 #### Flow though lock at Hvide Sande ####
-url <- 'http://hyde.dk/Sflow/default_flow.asp'
-webpage <- read_html(url)
-dat <- html_nodes(webpage, xpath = "/html/body/div[2]/div[3]/script/text()")
-dat <- html_text(dat)
-dat <- str_replace_all(dat, "[\r\n\t]", "")
-
-dates <- str_replace(dat, "^.*labels:", "{\n'labels' :")
-dates <- str_replace(dates, "].*$", "]\n}")
-dates <- str_replace_all(dates, "'", '"')
-dates <- fromJSON(dates)$labels
-# dates
-
-flow <- str_replace(dat, "^.*data:", "{\n'data' :")
-flow <- str_replace(flow, "].*$", "]\n}")
-flow <- str_replace_all(flow, "'", '"')
-flow <- fromJSON(flow)$data
-# flow
-
-if (length(dates)==length(flow)) {
-  fn <- "data/data_skjern_flow_lock.csv"
-  dat <- tibble(DateTime = dmy_hm(dates, tz = "CET"), Flow = flow)  %>% 
-    arrange_all(desc) %>% distinct()
-  if (!file.exists(fn)) {
-    write_csv(dat, fn)
-    dat
-  } else {
-    datOld <- read_csv(fn, col_types = "Ti", locale = locale(tz = "CET"))
-    dat <- bind_rows(datOld,dat) %>% arrange_all(desc) %>% distinct()
-    dat
-    write_csv(dat, fn)
-  }
-}
+updateLockSkjern("data/data_skjern_flow_lock.csv")
 
 
 #### Catch records ####
-fixStringErrors <- function(places) {
-  places <- places %>% str_replace("Vorgod �", "Vorgod Å") %>% 
-    str_replace("Omme �", "Omme Å") %>% 
-    str_replace("�vre", "Øvre")
-  return(places)
-}
-
-
-updateCatch <- function(fn, species, start = year(now()), reset = F) {
-  foundId <- NULL
-  datOld <- NULL
-  if (file.exists(fn) & !reset) {
-    datOld <- read_csv(fn)
-    foundId <- datOld %>% pull(Id)
-  }
-  
-  noError <- function(code) {
-    tryCatch(code,
-             error = function(c) TRUE
-    )
-  }
-  
-  curY <- year(now())
-  dat <- NULL
-  for (y in start:curY) {
-    cat("Year:", y, "\n")
-    url <- str_c("http://skjernaasam.dk/catchreport/?getyear=", y, "&species=", species)
-    page <- read_html(url)
-    ids <- html_nodes(page, xpath = '//*[@id="report-list"]/tbody/tr/@data-id') %>% 
-      html_text() %>% as.numeric()
-    for (id in ids) {
-      if (id %in% foundId & !reset) next
-      url <- str_c("http://skjernaasam.dk/ajax/?action=getcatch&id=", id) 
-      while (TRUE) {
-        val <- noError(jsonlite::fromJSON(url))
-        if (!is.logical(val)) break
-      }
-      if (is.null(val$imageid)) val$imagefile = ""
-      val <- val %>% unlist()
-      dat <- bind_rows(val, dat)
-    }
-  }
-  
-  if (!is.null(dat)) {
-    dat <- dat %>% select("Id" = id, "Date" = date, "Length" = length_cm, "Weight" = weight_kg, "Sex" = sex, "Place" = location, "Cut" = cut_fin, "Net" = net_injury, "NetDesc" = injury_description, "Killed" = released, "Method" = method, "Notes" = notes, "Kelt" = kelt, "ReportDate" = report_date, "Name" = name, "Foto" = imagefile)
-    dat <- dat %>% 
-      mutate(Killed = if_else(Killed == "Ja", FALSE, TRUE),  # Since represent released
-             Length = str_replace_all(Length, c(".cm" = "", "Ukendt" = NA_character_)),
-             Weight = str_replace_all(Weight, c(".kg" = "", "Ukendt" = NA_character_)),
-             Cut = case_when(
-               Cut %in% c("Ja", "left_pelvic", "right_pelvic", "pelvic", "Fedtfinne", "tail", "pectoral", "anal") ~ TRUE, 
-               Cut == "Nej" ~ FALSE,
-               TRUE ~ NA),
-             Net = if_else(Net == "Ja", TRUE, FALSE),
-             Place = case_when(
-               str_detect(Place, "Øvre|Rind|Karstoft|Vinbæk|Opstrøms") ~ "Øvre",
-               str_detect(Place, "Mellem|Borris Krog bro til Tarp Bro|Felding|Konsortiet") ~ "Mellem",
-               str_detect(Place, "Nedre|A11|Albæk|Fjord") ~ "Nedre",
-               str_detect(Place, "Vorgod") ~ "Vorgod Å",
-               str_detect(Place, "Omme") ~ "Omme Å",
-               TRUE ~ "Andet"),
-             NetDesc = str_to_sentence(NetDesc),
-             Sex = str_replace_all(Sex, c("Ukendt" = NA_character_)),
-             Name = str_to_title(Name),
-             Notes = str_to_sentence(Notes)) %>% 
-      type_convert(col_types = cols(
-        Date = col_date(format = ""),
-        Length = col_double(),
-        Weight = col_double(),
-        Name = col_character(),
-        Place = col_character(),
-        Method = col_character(),
-        Sex = col_character(),
-        Cut = col_logical(),
-        Killed = col_logical(),
-        Foto = col_character(),
-        Notes = col_character(),
-        Net = col_logical(),
-        NetDesc = col_character(),
-        Kelt = col_character(),
-        ReportDate = col_datetime(format = ""),
-        Id = col_double()
-      )) %>% 
-      select(Date, Length, Weight, Name, Place, Method, Sex, Cut, Killed, Foto, Notes, 
-             Net, NetDesc, Kelt, ReportDate, Id)
-    
-    # Try to fix errors
-    dat <- dat %>% mutate(Length = if_else(Length < 40 & Weight > 0.5, NA_real_, Length))
-    # tmp <- dat %>% 
-    #   group_by(Date, Name, Place, Length, Weight, Method, Sex, Killed, Cut) %>% 
-    #   dplyr::filter(n()>1)
-    
-    if (reset & !is.null(datOld)) datOld <- datOld %>% filter(year(date) < start)
-    dat <- bind_rows(datOld,dat)
-    dat <- dat %>% 
-      dplyr::filter(Length > 39 | is.na(Length)) %>% 
-      mutate(Place = fixStringErrors(Place)) %>% 
-      arrange(desc(Date, ReportDate))
-    write_csv(dat, fn)
-  } else {
-    dat <- datOld
-  }
-  return(dat)
-}
-datCatchSalmon <- updateCatch("data/data_skjern_catch_salmon.csv", species = "salmon", reset = T, start = 2004)   #, reset = T, start = 2004
-datCatchSeatrout <- updateCatch("data/data_skjern_catch_seatrout.csv", species = "trout")
+datCatchSalmon <- updateCatchSkjern("data/data_skjern_catch_salmon.csv", species = "salmon", reset = T, start = 2004)   #, reset = T, start = 2004
+datCatchSeatrout <- updateCatchSkjern("data/data_skjern_catch_seatrout.csv", species = "trout")
 
 
 #### Salmon - Estimate weight given length ####
-datCKilled <- datCatchSalmon %>% 
-  mutate(Length = round(Length), Weight = round(Weight,1), Month = month(Date, label = T)) %>%
-  mutate(Period = factor(Month, ordered = F)) %>% 
-  dplyr::filter(Length > 39 & Killed) 
-mod <- lm(log(Weight) ~ Period*log(Length), datCKilled)
-datP <- expand_grid(Length = 40:145, Period = unique(datCKilled$Period))
-res <- predict(mod, datP, interval='prediction', level=0.95) 
-res <- exp(res)
-res <- res %>% as_tibble() 
-res <- bind_cols(datP, res) %>% group_by(Period) #%>% select(Length:Avg)
-colnames(res) <- c("Length", "Period", "Avg", "Lower", "Upper")
+# dat <- datCatchSalmon %>% 
+#   mutate(Period = factor(month(Date, label = T), ordered = F)) 
+# datWeightSalmon <- estimateWeight("data/data_skjern_weight_salmon.csv", dat, minLength = 40, maxLength = 145)
+# 
+# dat <- datCatchSeatrout %>% 
+#   mutate(Period = factor(month(Date, label = T), ordered = F)) 
+# datWeightSeatrout <- estimateWeight("data/data_skjern_weight_seatrout.csv", dat, minLength = 40)
 
-## Save to file
-fn <- "data/data_skjern_weight_salmon.csv"
-write_csv(res, fn)
+# datCKilled <- datCatchSalmon %>% 
+#   mutate(Length = round(Length), Month = month(Date, label = T)) %>%
+#   mutate(Period = factor(Month, ordered = F)) %>% 
+#   dplyr::filter(Length > 39 & Killed) 
+# mod <- lm(log(Weight) ~ Period*log(Length), datCKilled)
+# datP <- expand_grid(Length = 40:145, Period = unique(datCKilled$Period))
+# res <- predict(mod, datP, interval='prediction', level=0.95) 
+# res <- exp(res)t
+# res <- res %>% as_tibble() 
+# res <- bind_cols(datP, res) %>% group_by(Period) #%>% select(Length:Avg)
+# colnames(res) <- c("Length", "Period", "Avg", "Lower", "Upper")
+# 
+# ## Save to file
+# fn <- "data/data_skjern_weight_salmon.csv"
+# write_csv(res, fn)
 
 
 #### Seatrout - Estimate weight given length ####
-datCKilled <- datCatchSeatrout %>% 
-  mutate(Length = round(Length), Weight = round(Weight,1), Month = month(Date, label = T)) %>%
-  mutate(Period = factor(Month, ordered = F)) %>% 
-  dplyr::filter(Length > 39 & Killed) 
-mod <- lm(log(Weight) ~ Period*log(Length), datCKilled)
-datP <- expand_grid(Length = 40:max(datCKilled$Length), Period = unique(datCKilled$Period))
-res <- predict(mod, datP, interval='prediction', level=0.95) 
-res <- exp(res)
-res <- res %>% as_tibble() 
-res <- bind_cols(datP, res) %>% group_by(Period) #%>% select(Length:Avg)
-colnames(res) <- c("Length", "Period", "Avg", "Lower", "Upper")
-
-## Save to file
-fn <- "data/data_skjern_weight_seatrout.csv"
-write_csv(res, fn)
+# datCKilled <- datCatchSeatrout %>% 
+#   mutate(Length = round(Length), Weight = round(Weight,1), Month = month(Date, label = T)) %>%
+#   mutate(Period = factor(Month, ordered = F)) %>% 
+#   dplyr::filter(Length > 39 & Killed) 
+# mod <- lm(log(Weight) ~ Period*log(Length), datCKilled)
+# datP <- expand_grid(Length = 40:max(datCKilled$Length), Period = unique(datCKilled$Period))
+# res <- predict(mod, datP, interval='prediction', level=0.95) 
+# res <- exp(res)
+# res <- res %>% as_tibble() 
+# res <- bind_cols(datP, res) %>% group_by(Period) #%>% select(Length:Avg)
+# colnames(res) <- c("Length", "Period", "Avg", "Lower", "Upper")
+# 
+# ## Save to file
+# fn <- "data/data_skjern_weight_seatrout.csv"
+# write_csv(res, fn)
 
 
 # #### Waterlevel - Prelim data set ####
@@ -276,6 +153,9 @@ write_csv(res, fn)
 #   dat2 <- dat1 %>% dplyr::filter(year(Date) == y) %>% arrange(Date)
 #   write_csv(dat2, fn)
 # }
+
+
+
 
 
 #### Waterlevel - Update data current year ####
