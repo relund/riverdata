69f299408a0f82520de31c06311f0d336c18b85d
Lars Relund Nielsen <junk@relund.dk>
2021-01-06 13:35:03 +0100

    Updated Skjern shiny app

---
+++ shiny/skjern/functions.R
@@ -0,0 +1,201 @@
+#' Read and prepare catch records for table
+#'
+#' @param path Full path to csv file.
+#' @param datWeight Weight estimates
+#'
+#' @return The catch records
+readCatch <- function(path, datWeight) {
+  datCatch <- read_csv(path) %>% 
+    mutate(
+      Misc = paste0(
+        if_else(!Killed, "<img src=\"www/c_and_r.gif\" alt=\"C&R\">", "", ""),
+        if_else(Cut, "<img src=\"www/cut.gif\" alt=\"Finneklippet\">", "", ""),
+        if_else(Sex == 'Han', '<img src="www/boy.gif" alt="Han">', "", ""),
+        if_else(Sex == 'Hun', '<img src="www/girl.gif" alt="Hun">', "", ""),
+        if_else(Net, '<img src="www/net.gif" alt="Garnskadet">', "", ""),
+        if_else(!is.na(Foto),str_c("<a href=\"", Foto, "\", target=\"_blank\"><img src=\"www/foto.gif\" alt=\"Foto\"></a>"),"", "")
+      ),
+      Month = month(Date, label = T), Week = week(Date), Year = year(Date), 
+      NoWeight = 1*is.na(Weight), Day = yday(Date), DayStr = format(Date, "%d. %b")
+    )
+  datCatch <- left_join(datCatch, datWeight, by = c("Length", "Month" = "Period")) %>% 
+    mutate(Weight = if_else(is.na(Weight), round(Avg,1), Weight)) %>% 
+    mutate(Fulton = Weight*100000/Length^3) %>% 
+    mutate(Month = month(Date, label = T))
+  return(datCatch)
+}
+
+
+#' Calculate yearly catch statistics
+#'
+#' @param datCatch Catch records.
+#'
+#' @return Yearly catch statistics.
+yearlyStat <- function(datCatch) {
+  dat <- datCatch %>% 
+    mutate(Year = year(Date)) %>% group_by(Year) %>% nest() %>% 
+    mutate(
+      TotalStat = map(data, function(df) {
+        summarise(df, Total = n(), 
+                  Female = sum(Sex == "Hun", na.rm = T), 
+                  Male = sum(Sex == "Han", na.rm = T),
+                  SexUnknown = Total - Female - Male,
+                  Released = sum(!Killed, na.rm = T),
+                  Killed = sum(Killed, na.rm = T),
+                  KilledUnknown = Total - Released - Killed,
+                  LengthAvg = mean(Length, na.rm = T), 
+                  LengthMax = max(Length, na.rm = T),
+                  WeightAvg = mean(Weight, na.rm = T), 
+                  WeightMax = max(Weight, na.rm = T),
+                  Kg = sum(Weight, na.rm = T),
+                  FultonAvg = mean(Fulton, na.rm = T), 
+                  FultonMax = max(Fulton, na.rm = T)
+        )
+      }),
+      PlaceStat = 
+        map(data, 
+            function(df) {
+              df %>% 
+                group_by(Place) %>% 
+                summarize(TotalP = n(), 
+                          KilledP = sum(Killed))}),
+      MethodStat = 
+        map(data, 
+            function(df) {
+              df %>% 
+                group_by(Method) %>% 
+                summarize(TotalM = n())})
+    )
+  
+  dat <- dat %>% 
+    mutate(PlaceStat = 
+             map(PlaceStat, function(df) {  
+               pivot_wider(df, names_from = Place, values_from = c(TotalP, KilledP))}),
+           MethodStat = 
+             map(MethodStat, function(df) {  
+               pivot_wider(df, names_from = Method, values_from = c(TotalM))})
+    ) %>% 
+    unnest(cols = c(TotalStat, PlaceStat, MethodStat)) %>% select(-data) %>% 
+    replace(., is.na(.), 0)
+  
+  dat <- 
+    dat  %>% 
+    ungroup() %>% 
+    transmute(Year, Total, 
+              Sex = paste0(format(100*Male/Total, digits = 0, trim = TRUE), "/",
+                           format(100*Female/Total, digits = 0, trim = TRUE), "/",
+                           format(100*SexUnknown/Total, digits = 0, trim = TRUE)),
+              Place = paste0(format(100*TotalP_Nedre/Total, digits = 0, trim = TRUE), "/", 
+                             format(100*TotalP_Mellem/Total, digits = 0, trim = TRUE), "/", 
+                             format(100*`TotalP_Øvre`/Total, digits = 0, trim = TRUE), "/", 
+                             format(100*`TotalP_Vorgod Å`/Total, digits = 0, trim = TRUE), "/",
+                             format(100*`TotalP_Omme Å`/Total, digits = 0, trim = TRUE), "/",
+                             format(100*`TotalP_Andet`/Total, digits = 0, trim = TRUE)),
+              PlaceK = paste0(format(100*KilledP_Nedre/Killed, digits = 0, trim = TRUE), "/", 
+                              format(100*KilledP_Mellem/Killed, digits = 0, trim = TRUE), "/", 
+                              format(100*`KilledP_Øvre`/Killed, digits = 0, trim = TRUE), "/", 
+                              format(100*`KilledP_Vorgod Å`/Killed, digits = 0, trim = TRUE), "/",
+                              format(100*`KilledP_Omme Å`/Killed, digits = 0, trim = TRUE), "/",
+                              format(100*`KilledP_Andet`/Killed, digits = 0, trim = TRUE)),
+              Method = paste0(format(100*Flue/Total, digits = 0, trim = TRUE), "/", 
+                              format(100*Spin/Total, digits = 0, trim = TRUE), "/", 
+                              format(100*Orm/Total, digits = 0, trim = TRUE), "/", 
+                              format(100*Andet/Total, digits = 0, trim = TRUE)),
+              Released = paste0(round(100*Released/Total, 0), "/", 
+                                round(100*Killed/Total, 0)),
+              Length = paste0(round(LengthAvg,0), "/", round(LengthMax,0)), 
+              Weight = paste0(round(WeightAvg,1), "/", round(WeightMax,1), "/", round(Kg,0)), 
+              Fulton = paste0(round(FultonAvg,2), "/", round(FultonMax,2))) %>%
+    mutate_if(is.character, str_replace_all, pattern = "NaN|NA", replacement = "0")
+}
+
+
+
+#' Calculate monthly catch statistics
+#'
+#' @param datCatch Catch records.
+#' @param year Year under consideration.
+#'
+#' @return Monthly catch statistics.
+monthlyStat <- function(datCatch, year) {
+  dat <- datCatch %>% 
+    filter(year(Date) == year) %>% 
+    mutate(Month = month(Date)) %>% group_by(Month) %>% nest() %>% 
+    mutate(keep = map_lgl(data, function(df) { # remove months where no weight or length
+      if_else(nrow(df) == sum(is.na(df$Length)) | nrow(df) == sum(is.na(df$Weight)), FALSE, TRUE)
+    })) %>% 
+    filter(keep) %>% 
+    mutate(
+      TotalStat = map(data, function(df) {
+        summarise(df, Total = n(), 
+                  Female = sum(Sex == "Hun", na.rm = T), 
+                  Male = sum(Sex == "Han", na.rm = T),
+                  SexUnknown = Total - Female - Male,
+                  Released = sum(!Killed, na.rm = T),
+                  Killed = sum(Killed, na.rm = T),
+                  KilledUnknown = Total - Released - Killed,
+                  LengthAvg = mean(Length, na.rm = T), 
+                  LengthMax = max(Length, na.rm = T),
+                  WeightAvg = mean(Weight, na.rm = T), 
+                  WeightMax = max(Weight, na.rm = T),
+                  Kg = sum(Weight, na.rm = T),
+                  FultonAvg = mean(Fulton, na.rm = T), 
+                  FultonMax = max(Fulton, na.rm = T)
+        )
+      }),
+      PlaceStat = 
+        map(data, 
+            function(df) {
+              df %>% 
+                group_by(Place) %>% 
+                summarize(TotalP = n(), 
+                          KilledP = sum(Killed))}),
+      MethodStat = 
+        map(data, 
+            function(df) {
+              df %>% 
+                group_by(Method) %>% 
+                summarize(TotalM = n())})
+    )
+  
+  dat <- dat %>% 
+    mutate(PlaceStat = 
+             map(PlaceStat, function(df) {  
+               pivot_wider(df, names_from = Place, values_from = c(TotalP, KilledP))}),
+           MethodStat = 
+             map(MethodStat, function(df) {  
+               pivot_wider(df, names_from = Method, values_from = c(TotalM))})
+    ) %>% 
+    unnest(cols = c(TotalStat, PlaceStat, MethodStat)) %>% select(-data) %>% 
+    replace(., is.na(.), 0)
+  
+  dat <- 
+    dat  %>% 
+    ungroup() %>% 
+    transmute(Month, Total, 
+              Sex = paste0(format(100*Male/Total, digits = 0, trim = TRUE), "/",
+                           format(100*Female/Total, digits = 0, trim = TRUE), "/",
+                           format(100*SexUnknown/Total, digits = 0, trim = TRUE)),
+              Place = paste0(format(100*TotalP_Nedre/Total, digits = 0, trim = TRUE), "/", 
+                             format(100*TotalP_Mellem/Total, digits = 0, trim = TRUE), "/", 
+                             format(100*`TotalP_Øvre`/Total, digits = 0, trim = TRUE), "/", 
+                             format(100*`TotalP_Vorgod Å`/Total, digits = 0, trim = TRUE), "/",
+                             format(100*`TotalP_Omme Å`/Total, digits = 0, trim = TRUE), "/",
+                             format(100*`TotalP_Andet`/Total, digits = 0, trim = TRUE)),
+              PlaceK = paste0(format(100*KilledP_Nedre/Killed, digits = 0, trim = TRUE), "/", 
+                              format(100*KilledP_Mellem/Killed, digits = 0, trim = TRUE), "/", 
+                              format(100*`KilledP_Øvre`/Killed, digits = 0, trim = TRUE), "/", 
+                              format(100*`KilledP_Vorgod Å`/Killed, digits = 0, trim = TRUE), "/",
+                              format(100*`KilledP_Omme Å`/Killed, digits = 0, trim = TRUE), "/",
+                              format(100*`KilledP_Andet`/Killed, digits = 0, trim = TRUE)),
+              Method = paste0(format(100*Flue/Total, digits = 0, trim = TRUE), "/", 
+                              format(100*Spin/Total, digits = 0, trim = TRUE), "/", 
+                              format(100*Orm/Total, digits = 0, trim = TRUE), "/", 
+                              format(100*Andet/Total, digits = 0, trim = TRUE)),
+              Released = paste0(round(100*Released/Total, 0), "/", 
+                                round(100*Killed/Total, 0)),
+              Length = paste0(round(LengthAvg,0), "/", round(LengthMax,0)), 
+              Weight = paste0(round(WeightAvg,1), "/", round(WeightMax,1), "/", round(Kg,0)), 
+              Fulton = paste0(round(FultonAvg,2), "/", round(FultonMax,2))) %>%
+    mutate_if(is.character, str_replace_all, pattern = "NaN|NA", replacement = "0")
+}
