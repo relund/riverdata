a7e5767574aa11b8fc32fa2c69fa96b8cad33a46
Lars Relund Nielsen <junk@relund.dk>
2021-01-31 20:52:59 +0100

    Remove install

--- install.R
+++
@@ -1,7 +0,0 @@
-## Add R packages to install
-install.packages("xml2")
-install.packages("rvest")
-install.packages("jsonlite")
-install.packages("tidyverse")
-install.packages("lubridate")
-install.packages("zoo")
