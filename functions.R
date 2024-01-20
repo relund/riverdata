e0ec769001d4a03439b03a8ae3146d3b3b02ecd1
Lars Relund <junk@relund.dk>
2024-01-20 15:18:51 +0100

    Fix bugs

--- functions.R
+++ functions.R
@@ -736,13 +736,18 @@ stripKml <- function(mapId, Club = NA, GroupNameMarkers = NULL, GroupNameLines =
         str_detect(Group, fixed('sten', ignore_case=TRUE)) ~ "rock.png",
         str_detect(Desc, fixed('hytte', ignore_case=TRUE)) ~ "cottage.png",
         str_detect(Desc, fixed('indhegning', ignore_case=TRUE)) ~ "fence.png",
-        str_detect(Desc, fixed('laksens hus', ignore_case=TRUE)) ~ "house.png",
+        str_detect(Desc, fixed('hus', ignore_case=TRUE)) ~ "house.png",
         str_detect(Desc, fixed('fiskekort', ignore_case=TRUE)) ~ "house.png",
         str_detect(Desc, fixed('p-plads', ignore_case=TRUE)) ~ "park.png",
         str_detect(Desc, fixed('parkering', ignore_case=TRUE)) ~ "park.png",
         str_detect(Desc, fixed('spang', ignore_case=TRUE)) ~ "footbridge.png",
         str_detect(Desc, fixed('toilet', ignore_case=TRUE)) ~ "wc.png",
         str_detect(Desc, fixed('wc', ignore_case=TRUE)) ~ "wc.png",
+        str_detect(Desc, fixed('info', ignore_case=TRUE)) ~ "infoplace.png",
+        str_detect(Desc, fixed('shelter', ignore_case=TRUE)) ~ "shelter.png",
+        str_detect(Desc, fixed('bro', ignore_case=TRUE)) ~ "bridge.png",
+        str_detect(Desc, fixed('båd', ignore_case=TRUE)) ~ "boat.png",
+        str_detect(Desc, fixed('fiskeret', ignore_case=TRUE)) ~ "infoplace.png",
         TRUE ~ NA_character_
       )) 
     if (!is.null(GroupNameMarkers)) datMarkers$Group <- GroupNameMarkers
--- script_mv-lf.R
+++ script_mv-lf.R
@@ -15,15 +15,37 @@ prefix <- "data/data_mv-lf"
 # yr <- year(now())
 
 #### Map ####
-
-# MV-LF
-lst <- stripKml("1_xtzzPTUUiecahNG71C_4rOY_izwVmU", Club = "MV-LF") # storå
+# Stednavne Karup
+lst <- stripKml("1XJoAUKY_-kbmhZgovPpLgi82Gn8")  
 datMarkers <- lst$datMarkers
 datLines <- lst$datLines
 
-lst <- stripKml("14vdqfI93frzgB0jmm44GPP3bIu5aVH8", Club = "MV-LF") # lake fishing
+# Stednavne Skjern
+lst <- stripKml("135J9l0kVoBKkdIdG_0vc3U9WJeuUPWyJ")  # Places
 datMarkers <- bind_rows(datMarkers, lst$datMarkers) 
 datLines <- bind_rows(datLines, lst$datLines)
+
+# Karup
+lst <- stripKml("1A1Oi7hPeFbAU2ahS_zWgg6Fqcxa5Gbg", Club = "MV-LF")
+datMarkers <- bind_rows(datMarkers, lst$datMarkers) 
+datLines <- bind_rows(datLines, lst$datLines)
+
+## Skjern
+lst <- stripKml("1NiY_55Mw_GUULepLXq6Wjt0Gtu2K2c0", Club = "MV-LF")
+datMarkers <- bind_rows(datMarkers, lst$datMarkers) 
+datLines <- bind_rows(datLines, lst$datLines)
+
+# Storå
+lst <- stripKml("1_xtzzPTUUiecahNG71C_4rOY_izwVmU", Club = "MV-LF") 
+datMarkers <- bind_rows(datMarkers, lst$datMarkers) 
+datLines <- bind_rows(datLines, lst$datLines)
+
+# Lake
+lst <- stripKml("14vdqfI93frzgB0jmm44GPP3bIu5aVH8", Club = "MV-LF") 
+datMarkers <- bind_rows(datMarkers, lst$datMarkers) 
+datLines <- bind_rows(datLines, lst$datLines)
+
+
 
 datMarkers <- datMarkers  %>% filter(!is.na(Icon))
 write_csv(datMarkers, str_c(prefix, "_mapmarkers.csv"))
