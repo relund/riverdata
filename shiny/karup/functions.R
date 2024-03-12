adeec7b996950331345d5eac4a82d048221b290c
Lars Relund <junk@relund.dk>
2024-03-12 11:08:41 +0100

    Fix bugs

--- shiny/karup/functions.R
+++ shiny/karup/functions.R
@@ -100,7 +100,7 @@ yearlyStat <- function(datCatch) {
   
   dat <- 
     # check for missing columns
-    if (!("TotalP_Andet" %in% names(dat))) dat <- dat %>% mutate(TotalP_Andet = 0, KilledP_Andet = 0)
+    if (!("TotalP_Ukendt" %in% names(dat))) dat <- dat %>% mutate(TotalP_Ukendt = 0, KilledP_Ukendt = 0)
   dat <- 
     dat  %>% 
     ungroup() %>% 
@@ -111,8 +111,8 @@ yearlyStat <- function(datCatch) {
               Place = paste0(round(100*Nedre/Total, 0), "/", 
                              round(100*Mellem/Total, 0), "/", 
                              round(100*`Øvre`/Total, 0), "/", 
-                             round(100*`Haderup Å`/Total, 0), "/",
-                             round(100*(Total - Nedre - Mellem - `Øvre` - `Haderup Å`)/Total, 0)),
+                             round(100*`Haderis Å`/Total, 0), "/",
+                             round(100*(Total - Nedre - Mellem - `Øvre` - `Haderis Å`)/Total, 0)),
               Method = paste0(round(100*Flue/Total, 0), "/", 
                               round(100*Spin/Total, 0), "/", 
                               round(100*Orm/Total, 0), "/", 
@@ -200,7 +200,7 @@ monthlyStat <- function(datCatch, year) {
   } else {
     # add missing cols
     cNames <- c("Month", "Total", "Male", "Female", "SexUnknown", "Nedre", "Mellem", "Øvre", 
-                "Haderup Å", "Flue", "Spin", "Orm", "Released", "LengthAvg", "LengthMax", 
+                "Haderis Å", "Flue", "Spin", "Orm", "Released", "LengthAvg", "LengthMax", 
                 "WeightAvg", "WeightMax", "FultonAvg", "FultonMax")
     cNames <- cNames[!(cNames %in% names(dat))]
     cols <- rep(0, length(cNames))
@@ -216,8 +216,8 @@ monthlyStat <- function(datCatch, year) {
                 Place = paste0(round(100*Nedre/Total, 0), "/", 
                                round(100*Mellem/Total, 0), "/", 
                                round(100*`Øvre`/Total, 0), "/", 
-                               round(100*`Haderup Å`/Total, 0), "/",
-                               round(100*(Total - Nedre - Mellem - `Øvre` - `Haderup Å`)/Total, 0)),
+                               round(100*`Haderis Å`/Total, 0), "/",
+                               round(100*(Total - Nedre - Mellem - `Øvre` - `Haderis Å`)/Total, 0)),
                 Method = paste0(round(100*Flue/Total, 0), "/", 
                                 round(100*Spin/Total, 0), "/", 
                                 round(100*Orm/Total, 0), "/", 
