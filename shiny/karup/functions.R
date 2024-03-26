cddeea2af904455fc711a85bc5907c17fac50ec8
Lars Relund <junk@relund.dk>
2024-03-26 21:58:14 +0100

    Fix weight shiny

--- shiny/karup/functions.R
+++ shiny/karup/functions.R
@@ -33,13 +33,13 @@ readCatch <- function(prefix, datWeight) {
                   "-",
                   formatC(MDay, width = 2, flag = "0"))
     )
-  datWeight <- datWeight %>%
-    mutate(Period = as.character(Period)) %>%
-    mutate(Period =
-             case_when(Period == "May" ~ "Maj",
-                       Period == "Oct" ~ "Okt",
-                       TRUE ~ Period))
-  datCatch <- left_join(datCatch, datWeight, by = c("Length", "Month" = "Period")) %>% 
+  # datWeight <- datWeight %>%
+  #   mutate(Period = as.character(Period)) %>%
+  #   mutate(Period =
+  #            case_when(Period == "May" ~ "Maj",
+  #                      Period == "Oct" ~ "Okt",
+  #                      TRUE ~ Period))
+  datCatch <- left_join(datCatch, datWeight, by = c("Length", "MonthN"))  %>% 
     mutate(Weight = if_else(is.na(Weight), round(Avg,1), Weight)) %>% 
     mutate(Fulton = Weight*100000/Length^3) %>% 
     mutate(Month = month(Date, label = T))
