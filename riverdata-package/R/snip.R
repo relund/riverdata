3d9bccbe27689c120a992134d656a9fda0be65ff
Lars Relund <junk@relund.dk>
2026-03-10 22:05:59 +0100

    Update snip

--- riverdata-package/R/snip.R
+++ riverdata-package/R/snip.R
@@ -4,7 +4,8 @@
 #' Plot of catches given current date
 #'
 #' @param datCatch Catch data.
-#' @param .days Number of days to plot.
+#' @param .look_back Number of time units to plot.
+#' @param .unit Time unit for the look-back window. One of `"days"`, `"weeks"` or `"months"`.
 #' @param .plotly Use plotly otherwise just ggplot.
 #'
 #' @return The plot object.
@@ -13,39 +14,140 @@
 #' @examples
 #' dat <- read_data("data_karup_catch_seatrout_", year = 2023)
 #' snip_plot_catch(dat)
-snip_plot_catch <- function(datCatch, .days = 30, .plotly = TRUE) {
+snip_plot_catch <- function(datCatch, .look_back = 30, .unit = c("days", "weeks", "months"), .plotly = TRUE) {
+   .unit <- match.arg(.unit)
+
+   current_date <- today(tzone = "CET")
+   current_year <- max(datCatch$Year, na.rm = TRUE)
+   if (current_year < year(current_date)) {
+      current_date <- max(datCatch$Date, na.rm = TRUE) + days(1)
+   }
+   filter_threshold <- if (.unit == "weeks") {
+      current_date - weeks(.look_back)
+   } else if (.unit == "months") {
+      current_date - months(.look_back)
+   } else {
+      current_date - days(.look_back)
+   }
    dat <- datCatch %>%
-      dplyr::filter(Date > now(tzone = "CET") - days(.days)) %>%
-      select(Date, .data$Place) %>%
+      dplyr::filter(.data$Year == current_year, Date > filter_threshold) %>%
+      select(Date, Place) %>%
       arrange(desc(Date))
-   lastDate <- date(now() + ddays(1))
+
+   if (.unit == "weeks") {
+      lastDate <- floor_date(current_date, unit = "week", week_start = 1) + weeks(1)
+   } else if (.unit == "months") {
+      lastDate <- floor_date(current_date, unit = "month") + months(1)
+   } else {
+      lastDate <- current_date + days(1)
+   }
+
    if (nrow(dat) == 0) {  # find last days with catches
       dat <- datCatch %>%
-         mutate(DayCtr = max(Date) - Date) %>%
-         dplyr::filter(.data$DayCtr <= .days) %>%
+         dplyr::filter(.data$Year == current_year) %>%
+         dplyr::filter(Date > max(Date) - if (.unit == "weeks") {
+            weeks(.look_back)
+         } else if (.unit == "months") {
+            months(.look_back)
+         } else {
+            days(.look_back)
+         }) %>%
          arrange(desc(Date))
-      lastDate <- max(dat$Date) + ddays(1)
+
+      if (.unit == "weeks") {
+         lastDate <- floor_date(max(dat$Date), unit = "week", week_start = 1) + weeks(1)
+      } else if (.unit == "months") {
+         lastDate <- floor_date(max(dat$Date), unit = "month") + months(1)
+      } else {
+         lastDate <- max(dat$Date) + days(1)
+      }
    }
+
+   if (.unit == "weeks") {
+      dat <- dat %>%
+         mutate(Date = floor_date(Date, unit = "week", week_start = 1))
+   } else if (.unit == "months") {
+      dat <- dat %>%
+         mutate(Date = floor_date(Date, unit = "month"))
+   }
+
    pt <- ggplot(data = dat, aes(x = Date)) +
-      geom_bar(aes(fill = .data$Place), width = 0.75) +
-      geom_text(aes(label = after_stat(count), y= after_stat(count) ), stat= "count", position = position_dodge(width = 1)) + #  vjust = -0.5
-      scale_x_date(date_labels = "%e. %b", date_breaks = "1 day",
-                   limits = c(NA_Date_, lastDate)) + # date(now()+ddays(1))
+      geom_bar(aes(fill = .data$Place)) +
+      geom_text(
+         stat = "count",
+         aes(y = after_stat(count), label = after_stat(count)),
+         vjust = -0.2,
+         position = position_dodge(width = 1)
+      ) +
       labs(fill = "") + xlab("") + ylab("") +
       theme(axis.text.x  = element_text(angle=45, hjust = 1, vjust = 1), legend.position="bottom")
-   if (.plotly) pt <- ggplotly(pt, tooltip = NULL) %>%  # , dynamicTicks = TRUE
-      layout(
-         xaxis = list(domain = c(0, 1), title = NA, tickformat = "%d %b"),
-         # yaxis = list(title = "Relativ vandstand"),
-         legend = list(orientation = 'h'),
-         #hovermode = "x unified",
-         dragmode = "orbit"
-      ) |>
-      config(
-         displayModeBar = FALSE,
-         displaylogo = FALSE,
-         modeBarButtonsToRemove = c("lasso2d", "toImage", "select2d", "hoverClosestCartesian", "hoverCompareCartesian")
-      )
+
+   if (.unit == "weeks") {
+      week_breaks <- seq(min(dat$Date, na.rm = TRUE), lastDate, by = "1 week")
+      pt <- pt +
+         scale_x_date(
+            breaks = week_breaks,
+            labels = function(x) {
+               labels <- paste("Uge", isoweek(x))
+               labels[x == max(x)] <- ""
+               labels
+            },
+            limits = c(NA_Date_, lastDate)
+         )
+   } else if (.unit == "months") {
+      month_breaks <- seq(min(dat$Date, na.rm = TRUE), lastDate, by = "1 month")
+      pt <- pt +
+         scale_x_date(
+            breaks = month_breaks,
+            labels = function(x) {
+               labels <- format(x, "%b")
+               labels[x == max(x)] <- ""
+               labels
+            },
+            limits = c(NA_Date_, lastDate)
+         )
+   } else {
+      day_breaks <- seq(min(dat$Date, na.rm = TRUE), lastDate, by = "1 day")
+      pt <- pt +
+         scale_x_date(
+            breaks = day_breaks,
+            labels = function(x) {
+               labels <- format(x, "%e. %b")
+               labels[x == max(x)] <- ""
+               labels
+            },
+            limits = c(NA_Date_, lastDate)
+         )
+   }
+
+   if (.plotly) {
+      pt <- ggplotly(pt, tooltip = c("count", "fill"))
+
+      for (i in seq_along(pt$x$data)) {
+         trace <- pt$x$data[[i]]
+         if (!identical(trace$type, "bar")) next
+         if (is.null(trace$x) || is.null(trace$y) || is.null(trace$name)) next
+
+         pt$x$data[[i]]$hovertext <- paste0(
+            "Antal: ", trace$y,
+            "<br>Område: ", trace$name
+         )
+         pt$x$data[[i]]$hovertemplate <- "%{hovertext}<extra></extra>"
+      }
+
+      pt <- pt %>%
+         layout(
+            # yaxis = list(title = "Relativ vandstand"),
+            legend = list(orientation = 'h'),
+            #hovermode = "x unified",
+            dragmode = "orbit"
+         ) |>
+         config(
+            displayModeBar = FALSE,
+            displaylogo = FALSE,
+            modeBarButtonsToRemove = c("lasso2d", "toImage", "select2d", "hoverClosestCartesian", "hoverCompareCartesian")
+         )
+   }
    return(pt)
 }
 
